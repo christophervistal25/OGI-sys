15266cc66e45286144e762fd100b5c27d5ff76e7
CHriss <christophervistal25@gmail.com>
2019-12-16 20:49:39 +0800

    Change subjects button in department for students

--- resources/views/admin/departments/includes/index_action.blade.php
+++ resources/views/admin/departments/includes/index_action.blade.php
@@ -1,4 +1,9 @@
 <div class="text-center">
-	<button class="btn btn-primary btn-sm text-center mr-3 text-white" onclick="viewSubjects(this)" data-src="{{ $department }}"><i class="fa fa-book"></i> Subjects  - <span class="font-weight-bold">{{ $department->subjects->count() }}</span></button>
+	
+	@if(Str::contains(url()->previous(), 'department/students'))
+		<a href="/admin/student/{{$department->id}}" class="btn btn-primary btn-sm"><i class="fa fa-users"></i> Students</a>
+		@else
+		<button class="btn btn-primary btn-sm text-center mr-3 text-white" onclick="viewSubjects(this)" data-src="{{ $department }}"><i class="fa fa-book"></i> Subjects  - <span class="font-weight-bold">{{ $department->subjects->count() }}</span></button>
+    @endif
 	<button class="btn btn-success btn-sm text-center mr-3 text-white" onclick="editDepartment(this)" data-src="{{ $department }}"><i class="fa fa-edit"></i> Edit</button>
 </div>
