62434a69670e6b1c6ab875bf7a9dadaf83c7103f
CHriss <christophervistal25@gmail.com>
2020-01-15 11:23:42 +0800

    Add title to all buttons

--- resources/views/admin/student/includes/index_action.blade.php
+++ resources/views/admin/student/includes/index_action.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center">
-	<a href="{{ route('student.subject.create', [$student]) }}" class="btn btn-primary btn-sm text-center mr-3"><i class="fa fa-plus"></i></a>
-	<a href="{{ route('student.grade.show', [$student]) }}" class="btn btn-primary btn-sm text-center mr-3"><i class="fa fa-eye"></i></a>
-	<a href="{{ route('student.edit', [$student]) }}" class="btn btn-success btn-sm text-center mr-3"><i class="fa fa-edit"></i></a>
-	<button class="btn btn-info btn-sm btn-white text-center text-white" onclick="printGrade(this)" data-student-id="{{$student->id}}"><i class="fa fa-print" style="pointer-events: none;"></i></button>
+	<a href="{{ route('student.subject.create', [$student]) }}" title="Add Subject" class="btn btn-primary btn-sm text-center mr-3"><i class="fa fa-plus"></i></a>
+	<a href="{{ route('student.grade.show', [$student]) }}" title="View grade" class="btn btn-primary btn-sm text-center mr-3"><i class="fa fa-eye"></i></a>
+	<a href="{{ route('student.edit', [$student]) }}" title="Student Edit" class="btn btn-success btn-sm text-center mr-3"><i class="fa fa-edit"></i></a>
+	<button class="btn btn-info btn-sm btn-white text-center text-white" title="Print Grade" onclick="printGrade(this)" data-student-id="{{$student->id}}"><i class="fa fa-print" style="pointer-events: none;"></i></button>
 </div>
