1adf5138fde719899ff6668b610550d19364995e
CHriss <christophervistal25@gmail.com>
2020-02-03 20:03:00 +0800

    Fix instructor subject in admin

--- app/Http/Controllers/Admin/InstructorSubjectController.php
+++ app/Http/Controllers/Admin/InstructorSubjectController.php
@@ -15,10 +15,11 @@ class InstructorSubjectController extends Controller
 
     public function show(int $id)
     {
-    	$instructor = Instructor::with(['subjects', 'subjects.students' => function ($query) use($id) 
+        $instructor = Instructor::find($id);
+    	/*$instructor = Instructor::with(['subjects', 'subjects.students' => function ($query) use($id) 
     	{
     		$query->where('instructor_id', $id);
-    	}])->find($id);
+    	}])->find($id);*/
     	return view('admin.instructor.subject.show', compact('instructor'));
     }
 }
